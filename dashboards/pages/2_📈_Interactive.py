516ab1a1b403fd917cc96c3b7ae353539fc54a5e
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2024-02-20 17:48:35 -0300

    Refact: Dataset is now loaded only in main script
    Also, the interactive dashboard is now a subpage.

---
+++ dashboards/pages/2_📈_Interactive.py
@@ -0,0 +1,36 @@
+import plotly.express as px
+import streamlit as st
+
+
+st.set_page_config(layout="wide", page_title="Interactive Dashboard", page_icon=":chart_with_upwards_trend:")
+
+st.title('Rent Pricing at American Districts')
+col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
+
+st.sidebar.title('Interactive Graphs')
+st.sidebar.header('Parameters')
+
+interactive_dataset = st.session_state['df'].copy()
+
+boroughs = st.sidebar.multiselect('Select the borough', interactive_dataset["Neighborhood"].unique())
+neighbor = interactive_dataset[interactive_dataset["Neighborhood"].isin(boroughs)]
+
+graph1 = neighbor.groupby('Neighborhood')[['Price']].mean().reset_index()
+fig = px.bar(graph1, x='Neighborhood', y='Price', color='Neighborhood', title="Average price per district")
+col1.plotly_chart(fig, use_container_width=True)
+
+graph2 = neighbor.groupby('Neighborhood')[['Days Available']].mean().reset_index()
+fig = px.bar(graph2, x='Neighborhood', y='Days Available', color='Neighborhood', title='Average availability per district')
+col2.plotly_chart(fig, use_container_width=True)
+
+room_type = st.sidebar.multiselect('Select the room type', interactive_dataset["Room Type"].unique())
+rooms = interactive_dataset[interactive_dataset["Room Type"].isin(room_type)]
+
+graph3 = rooms.groupby('Room Type')[['Price']].mean().reset_index()
+fig = px.bar(graph3, x='Room Type', y='Price', color='Room Type', title="Average price per room type")
+col3.plotly_chart(fig, use_container_width=True)
+
+graph4 = rooms.groupby('Room Type')[['Days Available']].mean().reset_index()
+fig = px.bar(graph4, x='Room Type', y='Days Available', color='Room Type', title="Average availability per room type")
+col4.plotly_chart(fig, use_container_width=True)
