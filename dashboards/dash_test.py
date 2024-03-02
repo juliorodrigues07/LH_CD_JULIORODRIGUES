d8a9d00ece5ed2873db89c67a56fdcf6d768c122
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2024-03-02 20:15:08 -0300

    Update: Column name changed

--- dashboards/dash_test.py
+++ dashboards/dash_test.py
@@ -14,7 +14,7 @@ df = df.rename(columns={
     'nome': 'Name',
     'host_id': 'Host ID',
     'host_name': 'Host Name',
-    'bairro_group': 'Neighborhood',
+    'bairro_group': 'Borough',
     'bairro': 'District',
     'latitude': 'Latitude',
     'longitude': 'Longitude',
@@ -29,8 +29,8 @@ df = df.rename(columns={
 })
 df = df.drop(['id'], axis='columns')
 
-static1 = df.groupby(('Neighborhood'))[['Monthly Reviews']].mean().reset_index()
-static2 = df.groupby(['Neighborhood', 'Room Type'])[['Minimum Nights']].mean().reset_index()
+static1 = df.groupby(('Borough'))[['Monthly Reviews']].mean().reset_index()
+static2 = df.groupby(['Borough', 'Room Type'])[['Minimum Nights']].mean().reset_index()
 static3 = df.groupby(['Minimum Nights'])[['Price']].mean().reset_index()
 static3['Price'] = np.log2(static3['Price'])
 static4 = df.groupby(['Days Available'])[['Price']].mean().reset_index()
@@ -40,11 +40,11 @@ app.layout = html.Div([
 
     html.H1("Rent Pricing at NY boroughs", style={'text-align': 'center'}),
 
-    html.H3("Interactive Graphs by districts", style={'text-align': 'center'}),
+    html.H3("Interactive Graphs by Boroughs", style={'text-align': 'center'}),
 
     html.Div([
-        'Select districts',
-        dcc.Dropdown(id="slct_district",
+        'Select Boroughs',
+        dcc.Dropdown(id="slct_borough",
                      options=[
                          {'label': 'Manhattan', 'value': 'Manhattan'},
                          {'label': 'Brooklyn', 'value': 'Brooklyn'},
@@ -91,22 +91,22 @@ app.layout = html.Div([
     html.H3("Static Graphs", style={'text-align': 'center', 'margin-top': '50px'}),
 
     html.Div(children=[
-        dcc.Graph(id='map5', figure=px.bar(static1, x='Neighborhood', y='Monthly Reviews', color='Neighborhood', title="Average montlhy reviews per district")),
-        dcc.Graph(id='map6', figure=px.bar(static2, x='Neighborhood', y='Minimum Nights', color='Room Type', title="Average minimum nights required"))
+        dcc.Graph(id='map5', figure=px.bar(static1, x='Borough', y='Monthly Reviews', color='Borough', title="Average Montlhy Reviews per Borough")),
+        dcc.Graph(id='map6', figure=px.bar(static2, x='Borough', y='Minimum Nights', color='Room Type', title="Average Minimum Nights Required"))
         ],
         style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly', 'padding': '20px'}
     ),
 
     html.Div(children=[
-        dcc.Graph(id='map7', figure=px.bar(static3, x='Minimum Nights', y='Price', title="Average price by minimum nights required",
+        dcc.Graph(id='map7', figure=px.bar(static3, x='Minimum Nights', y='Price', title="Average Price by Minimum Nights Required",
                                            labels={'Price': 'Price (log 2)'})),
-        dcc.Graph(id='map8', figure=px.bar(static4, x='Days Available', y='Price', title="Average price by availability"))
+        dcc.Graph(id='map8', figure=px.bar(static4, x='Days Available', y='Price', title="Average Price by Availability"))
     ],
         style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly', 'padding': '20px'}
     ),
 
     html.Div(children=[
-        dcc.Graph(id='map9', figure=px.pie(static5, values='Price', names='Room Type', title='Total receipt by room type'))
+        dcc.Graph(id='map9', figure=px.pie(static5, values='Price', names='Room Type', title='Total Receipt by Room Type'))
     ],
         style={'display': 'flex', 'justify-content': 'center', 'padding': '20px'}
     )
@@ -117,19 +117,19 @@ app.layout = html.Div([
 @app.callback(
     [Output(component_id='map1', component_property='figure'),
      Output(component_id='map2', component_property='figure')],
-    [Input(component_id='slct_district', component_property='value')]
+    [Input(component_id='slct_borough', component_property='value')]
 )
-def update_by_district(district_slctd):
+def update_by_borough(borough_slctd):
 
     neighbor = df.copy()
-    neighbor = neighbor[neighbor["Neighborhood"].isin(district_slctd)]
+    neighbor = neighbor[neighbor["Borough"].isin(borough_slctd)]
 
-    graph1 = neighbor.groupby(('Neighborhood'))[['Price']].mean().reset_index()
-    fig1 = px.bar(graph1, x='Neighborhood', y='Price', color='Neighborhood', title="Average price per district")
+    graph1 = neighbor.groupby(('Borough'))[['Price']].mean().reset_index()
+    fig1 = px.bar(graph1, x='Borough', y='Price', color='Borough', title="Average Price per Borough")
 
-    graph2 = neighbor.groupby(('Neighborhood'))[['Days Available']].mean().reset_index()
-    fig2 = px.bar(graph2, x='Neighborhood', y='Days Available', color='Neighborhood',
-                  title='Average availability per district')
+    graph2 = neighbor.groupby(('Borough'))[['Days Available']].mean().reset_index()
+    fig2 = px.bar(graph2, x='Borough', y='Days Available', color='Borough',
+                  title='Average Availability per Borough')
 
     return fig1, fig2
 
@@ -145,11 +145,11 @@ def update_by_roomtype(slct_roomtype):
     rooms = rooms[rooms["Room Type"].isin(slct_roomtype)]
 
     graph1 = rooms.groupby(('Room Type'))[['Price']].mean().reset_index()
-    fig1 = px.bar(graph1, x='Room Type', y='Price', color='Room Type', title="Average price per district")
+    fig1 = px.bar(graph1, x='Room Type', y='Price', color='Room Type', title="Average Price per Borough")
 
     graph2 = rooms.groupby(('Room Type'))[['Days Available']].mean().reset_index()
     fig2 = px.bar(graph2, x='Room Type', y='Days Available', color='Room Type',
-                  title='Average availability per district')
+                  title='Average Availability per Borough')
 
     return fig1, fig2
 
